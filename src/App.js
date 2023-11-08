8336b5d824157b4c80a27b90ec0a6b0e0b78713f
Loloko1234 <kowalczykmaciek220@gmail.com>
2023-11-08 22:29:42 +0100

    Gotowy Projekt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+document.title = "Color Flipper";
 function App() {
+  const [a, setA] = useState("rgb(0, 0, 0)");
+  let flip = () => {
+    let first = Math.floor(Math.random() * 256);
+    let second = Math.floor(Math.random() * 256);
+    let third = Math.floor(Math.random() * 256);
+    let a = `rgb(${first}, ${second}, ${third})`;
+    document.body.style.backgroundColor = a;
+    setA(a);
+    let h1Elements = document.getElementsByClassName("h1-app");
+    h1Elements[0].style.color = a;
+    let btn1 = document.getElementsByClassName("btn");
+    btn1[0].style.backgroundColor = a;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="h1-app">{a}</h1>
+      <button onClick={flip} className="btn">
+        Change Color
+      </button>
     </div>
   );
 }
